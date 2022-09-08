74b07f440c2ba6152ef4d077209107c08c2f6b9d
unknown <ademolababalola2003@gmail.com>
2022-09-08 23:11:50 +0100

    department

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import "./about.css";
+import aboutImage from "../../Assets/images/maskedHospital.png"
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <main>
+      <section className="section1">
+        <h3>About Us</h3>
 
-export default About
+        <div className="about_container">
+          <div className="about_text">
+            <p>
+              Welcome to Surgicare Consult Hospital, one of the leading medical
+              centers in the city. Since our establishment in the month of June
+              2011, Founded by Dr. Kanumuagi Muchibayi Viya. we have been
+              providing world-class medical services to all our patients and have
+              been recognized for providing exceptional medical care. We have 3
+              Able Doctors, 7 Dominant nurses, and 8 part-time Nurses. At
+              Surgicare consult, we provide a range of high-quality medical
+              services. From treating common infections to more serious and
+              chronic diseases.{" "}
+            </p>
+          </div>
+
+          <div className="container_about-image">
+            <img src={aboutImage} alt="about" />
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default About;
--- src/components/department/Department.jsx
+++ src/components/department/Department.jsx
@@ -1,9 +1,80 @@
-import React from 'react'
+import React from "react";
+import "./department.css";
+import Dept from "../../Assets/svgs/departmentSvg.svg";
 
 const Department = () => {
   return (
-    <div>Department</div>
-  )
-}
+    <section className="section2">
+      <main>
+        <h3>Our Departments</h3>
 
-export default Department
+        <div className="department_container">
+          <div className="department">
+            <div>
+              <img src={Dept} alt="" />
+            </div>
+            <div>Laboratory Department</div>
+          </div>
+          <div className="department">
+            <div>
+              <img src={Dept} alt="" />
+            </div>
+            <div>Surgical Department</div>
+          </div>
+          <div className="department">
+            <div>
+              <img src={Dept} alt="" />
+            </div>
+            <div>Pediatrics Department</div>
+          </div>
+
+          <div className="department">
+            <div>
+              <img src={Dept} alt="" />
+            </div>
+            <div>Radiology Department</div>
+          </div>
+          <div className="department">
+            <div>
+              <img src={Dept} alt="" />
+            </div>
+            <div>Paraclinical Department</div>
+          </div>
+          <div className="department">
+            <div>
+              <img src={Dept} alt="" />
+            </div>
+            <div>Reproductive/Fertility Department</div>
+          </div>
+
+          <div className="department">
+            <div>
+              <img src={Dept} alt="" />
+            </div>
+            <div>Pharmaceutical Department</div>
+          </div>
+          <div className="department">
+            <div>
+              <img src={Dept} alt="" />
+            </div>
+            <div>Patheological Department</div>
+          </div>
+          <div className="department">
+            <div>
+              <img src={Dept} alt="" />
+            </div>
+            <div>Physiotherapy Department</div>
+          </div>
+          <div className="department">
+            <div>
+              <img src={Dept} alt="" />
+            </div>
+            <div>Department of Medical advice & consultation</div>
+          </div>
+        </div>
+      </main>
+    </section>
+  );
+};
+
+export default Department;
