bf6f6e88ec5d3006456719154ae3b2cc5b70f445
unknown <ademolababalola2003@gmail.com>
2022-09-08 13:24:04 +0100

    ...

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Home from './components/home/Home';
+import About from './components/about/About';
+import Department from './components/department/Department';
+import Service from './components/services/Service';
+import Faq from './components/faq/Faq';
+
+
+function App() {
+  return (
+    <div className="App">
+      App
+    </div>
+  );
+}
+
+export default App;
